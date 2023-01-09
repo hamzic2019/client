c5d5d5f5c97b40d1a39ec90629c909016b0b3cac
hamzic2019 <hamzicharis96@gmail.com>
2023-01-09 01:30:44 +0100

    some style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React,{useState, createContext} from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/" element={<h1>HOME PAGE</h1>} />
-          <Route path="/about" element={<h1>ABOUT</h1>} />
+          <Route path="/search" element={<Search />} />
         </Routes>
     </div>
   )
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Navigation.css';
+import {useNavigate, Link} from 'react-router-dom';
 
-const Navigation = () => {
+const Navigation = (data) => {
+  const navigate = useNavigate();
+  
+  const datas = [
+    {
+        title: 'Haris je kupio'
+    },{
+        title: 'amina nije jela'
+    },{
+        title: 'trebaju se palacinci napraviti'
+    },
+  ]
+
+  const [activeSearch, setActiveSearch] = useState(false)
+  const [input, setInput] = useState('');
+  
+  const handelAsk = () => {
+    navigate('/search');
+    setActiveSearch(true);
+  }
+
+  useEffect(() => {
+    /*const result = datas.filter(item => {
+        return item.title.toLowerCase().includes(input.toLocaleLowerCase());
+    })
+    console.log(result); REAL TIME DATA SEARCH*/
+
+
+  }, [input])
+
   return (
     <div className='navigation-wrapper'>
         <div className="logo itms">
-            <h4>LOLx</h4>
+            <Link to="/" onClick={() => setActiveSearch(false)} className="link"><h4>VR Pijaca</h4></Link>
         </div>
 
-        <div className="navigation-search-and-btn itms">
-            <input placeholder='Pokušaj: Kuća od 25.000e' />
-            <button>PRETRAŽUJ</button>
+        <div className={`navigation-search-and-btn ${activeSearch && 'active-search'}`}>
+            <input 
+                className='search-and-btn-item1' 
+                placeholder='Pokušaj: Kuća od 25.000e'
+                onFocus={() => handelAsk()}
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+            />
+            <button className='search-and-btn-item2'>PRETRAŽUJ</button>
         </div>
 
         <div className="navigation-btns itms">
             <button className='navigation-btn-objavi'>OBJAVI OGLAS</button>
             <button className='navigation-btn-prijava'>PRIJAVA</button>
         </div>
+
     </div>
   )
 }
